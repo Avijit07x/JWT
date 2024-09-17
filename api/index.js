772b61b1fee08f4064f17bde821c0443b739acea
Avijit07x <deyavijit134@gmail.com>
2024-09-17 17:41:20 +0530

    fix

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 
 app.use(
 	cors({
-		origin: ["http://localhost:5173", "https://jwt-rho-dusky.vercel.app/"],
+		origin: ["http://localhost:5173"],
 		credentials: true,
 	})
 );
